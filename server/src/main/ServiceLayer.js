07c3c04ccd1eff0e7d269a7d64b950eae25680ba
yyarom112 <yaromy@post.bgu.ac.il>
2020-01-18 12:25:56 +0200

    done login ,logout & register

--- server/src/main/ServiceLayer.js
+++ server/src/main/ServiceLayer.js
@@ -1,42 +1,47 @@
 const CinemaSystem = require("./CinemaSystem");
 
 class ServiceLayer {
-  constructor() {
-    this.cinemaSystem = new CinemaSystem();
-    this.users = new Map();
-    this.users.set("admin", 0);
-    this.userCounter = 1;
-  }
-
-  register(userName, password, permissions) {
-    if (this.users.get(userName) !== "undefined") {
-      return "The user already Exist";
-    } else {
-      this.users.set(userName, this.userCounter);
-      this.userCounter++;
-      return this.cinemaSystem.register(
-        this.userCounter,
-        userName,
-        password,
-        permissions
-      );
+    constructor() {
+        this.cinemaSystem = new CinemaSystem();
+        this.users = new Map();
+        this.users.set("admin", 0);
+        this.userCounter = 1;
     }
-  }
 
-  login(userName, password) {
-    if (this.users.has(userName)) {
-      return this.cinemaSystem.login(
-        userName,
-        password,
-        this.users.get(userName)
-      );
+    register(userName, password, permissions) {
+        if (this.users.has(userName)) {
+            return "The user already Exist";
+        } else {
+            let result = this.cinemaSystem.register(
+                this.userCounter,
+                userName,
+                password,
+                permissions
+            );
+            if (result == 'The user registered successfully.') {
+                this.users.set(userName, this.userCounter);
+                this.userCounter++;
+            }
+            return result;
+        }
     }
-  }
 
-  logout(userName) {
-    if (this.users.has(userName)) {
-      return this.cinemaSystem.logout(this.users.get(userName));
+    login(userName, password) {
+        if (this.users.has(userName)) {
+            return this.cinemaSystem.login(
+                userName,
+                password,
+                this.users.get(userName)
+            );
+        }
+        return 'Incorrect user name.';
     }
-  }
+
+    logout(userName) {
+        if (this.users.has(userName)) {
+            return this.cinemaSystem.logout(this.users.get(userName));
+        }
+        return 'Incorrect user name.';
+    }
 }
 module.exports = ServiceLayer;
